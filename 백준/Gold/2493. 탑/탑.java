92eb4afef992cf8c857425c28813ca1badbbf8f0
Parkgeonmoo <50697545+Parkgeonmoo@users.noreply.github.com>
2023-08-01 00:03:27 +0900

    [Gold V] Title: 탑, Time: 868 ms, Memory: 101364 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        Stack<Top> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            int heigth = Integer.parseInt(st.nextToken());
+            if (stack.isEmpty()) {
+                sb.append("0" + " ");
+                stack.push(new Top(heigth,i));
+            }
+            else{
+                while(true) {
+                    if (stack.isEmpty()) {
+                        sb.append("0" + " ");
+                        stack.push(new Top(heigth,i));
+                        break;
+                    }
+                    else{
+                        Top top = stack.peek();
+                        if (top.height > heigth) {
+                            stack.push(new Top(heigth,i));
+                            sb.append(top.index+ " ");
+                            break;
+                        }
+                        else{
+                            stack.pop();
+                        }
+                    }
+                }
+            }
+
+        }
+        System.out.println(sb.toString());
+    }
+}
+class Top{
+    int height;
+    int index;
+
+    Top(int height,int index) {
+        this.height = height;
+        this.index = index;
+    }
+}
