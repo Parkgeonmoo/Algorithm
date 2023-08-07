0463c810e0be60db968e0ddb2d51bb7d59ffcda6
Parkgeonmoo <50697545+Parkgeonmoo@users.noreply.github.com>
2023-08-07 17:50:01 +0900

    [Silver III] Title: N과 M (3), Time: 428 ms, Memory: 68116 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int [] arr;
+    static int N;
+    static int M;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args)throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        find(0);
+        System.out.println(sb.toString());
+
+
+
+    }
+
+    public static void find(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            find(depth+1);
+        }
+    }
+}
