8343487b7b7e911fe68d3d0a7a8414203d92c044
Parkgeonmoo <50697545+Parkgeonmoo@users.noreply.github.com>
2023-08-08 16:42:18 +0900

    [Silver III] Title: N과 M (6), Time: 144 ms, Memory: 15920 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;
+    static int N;
+    static int M;
+    static int[] result;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        result = new int[M];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        find(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    public static void find(int depth, int start) {
+        if (depth == M) {
+            for (int temp : result) {
+                sb.append(temp + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            result[depth] = arr[i];
+            find(depth + 1, i + 1);
+        }
+    }
+}
