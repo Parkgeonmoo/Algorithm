b79179d970e897494e6a2bf30a44e202e5f2860c
Parkgeonmoo <50697545+Parkgeonmoo@users.noreply.github.com>
2023-08-10 21:20:38 +0900

    [Silver II] Title: N과 M (9), Time: 228 ms, Memory: 20980 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;
+    static int N;
+    static int M;
+    static int[] result;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        result = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        find(0);
+
+        System.out.println(sb.toString());
+    }
+
+    public static void find(int depth) {
+        if (depth == M) {
+            for (int temp : result) {
+                sb.append(temp).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;  // 이전에 선택한 원소 초기화
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i] && (prev == -1 || prev != arr[i])) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                prev = arr[i];
+                find(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
