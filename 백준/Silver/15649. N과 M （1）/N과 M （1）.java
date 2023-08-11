cd687bd55fcb77ed1a518d18862ad83c3f40976c
Parkgeonmoo <50697545+Parkgeonmoo@users.noreply.github.com>
2023-08-11 21:58:18 +0900

    [Silver III] Title: N과 M (1), Time: 328 ms, Memory: 34772 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,46 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int answer = 0;
-    static int[]result;
+
+
+    
+    static int[] result;
+    static int N;
+    static int M;
     static boolean[] visited;
-    public static void main(String[] args)throws IOException {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[]args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
         result = new int[M];
         visited = new boolean[N+1];
 
-        find(0,N,M);
+        find (0,0);
+        System.out.println(sb.toString());
+
+
 
 
     }
-    public static void find(int depth,int N,int M) {
+
+    public static void find(int depth,int index) {
+
         if (depth == M) {
             for (int temp : result) {
-                System.out.print(temp + " ");
+                sb.append(temp +" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
+
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
+                result[index] = i;
                 visited[i] = true;
-                result[depth] = i;
-                find(depth+1,N,M);
+                find(depth + 1, index + 1);
                 visited[i] = false;
             }
         }
     }
 
-    }
 
 
 
 
 
+}
