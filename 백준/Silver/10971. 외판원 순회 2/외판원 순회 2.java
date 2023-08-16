33e1ef848df77713ebad95229fee3e85f39040dc
Parkgeonmoo <pgw111111@naver.com>
2023-08-16 21:11:56 +0900

    [Silver II] Title: 외판원 순회 2, Time: 160 ms, Memory: 15176 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -4,58 +4,63 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static int N;
-    static boolean[] visited;
     static int[][] map;
+    static boolean[] visited;
+    static int cost = 0;
     static int Min = Integer.MAX_VALUE;
 
-    public static void main(String[]args)throws IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
 
-        for (int i =0 ; i < N; i++) {
+        map = new int[N][N];
+        visited = new boolean[N];
+
+
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++){
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-            visited = new boolean[N];
-            visited[0] = true;
-            find(0,0,0);
-
-
+        visited[0] = true;
+        dfs(0,0,0);
         System.out.println(Min);
 
 
+
+
     }
-    public static void find(int start,int now,int cost) {
+    public static void dfs(int start,int now,int cost) {
         if (allvisited()) {
-            if (map[now][start] != 0) {
-                Min = Math.min(Min,cost+map[now][start]);
+            if (map[now][start] != 0){
+                Min = Math.min(Min,cost + map[now][start]);
             }
             return;
         }
+
         for (int i = 1; i < N; i++) {
             if (!visited[i] && map[now][i] != 0) {
                 visited[i] = true;
-                find(start,i,cost+map[now][i]);
+                dfs(start,i,cost+map[now][i]);
                 visited[i] = false;
             }
         }
 
-    }
 
+    }
     public static boolean allvisited() {
         for (int i = 0; i < N; i++) {
-            if (!visited[i]){
+            if (!visited[i]) {
                 return false;
             }
         }
         return true;
     }
-
-
 }
+
