cef7289c02638c244d3d6803318dbfbb6aa99429
Parkgeonmoo <pgw111111@naver.com>
2023-08-18 10:31:05 +0900

    [Gold IV] Title: N-Queen, Time: 5288 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,41 +1,46 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static int N;
-    static int[] result;
-    static int answer = 0;
+    static int[] arr;
+    static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
 
-        N = Integer.parseInt(st.nextToken()); // 변수 N 초기화 수정
 
-        result = new int[N];
-        find(0);
+        dfs(0);
+
         System.out.println(answer);
-    }
 
-    public static void find(int row) {
+
+
+
+    }
+    public static void dfs(int row) {
         if (row == N) {
             answer++;
             return;
         }
-
-        for (int col = 0; col < N; col++) { // col 변수 수정
-            if (isValid(row, col)) {
-                result[row] = col;
-                find(row + 1);
+        for (int i = 0; i < N; i++) {
+            if (isValid(row, i)) {
+                arr[row] = i;
+                dfs(row + 1);
             }
         }
     }
-
-    public static boolean isValid(int row, int col) {
-        for (int prevRow = 0; prevRow < row; prevRow++) {
-            if (result[prevRow] == col || Math.abs(result[prevRow] - col) == Math.abs(prevRow - row)) {
+    public static boolean isValid(int row,int col) {
+        for (int prevrow = 0; prevrow < row; prevrow++) {
+            if (arr[prevrow] == col || Math.abs(arr[prevrow] - col) == Math.abs(prevrow - row)) {
                 return false;
             }
         }
