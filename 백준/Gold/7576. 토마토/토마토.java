b3cf7bb03a1a26b6075b6123fe77fee331086962
Parkgeonmoo <pgw111111@naver.com>
2023-08-24 10:43:45 +0900

    [Gold V] Title: 토마토, Time: 680 ms, Memory: 119308 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,31 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int N, M;
+    static int N,M;
     static int[][] map;
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
-    static boolean[][] visited;
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
+    static int answer;
 
-    static int countday = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        map = new int[M][N];
-        visited = new boolean[M][N];
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
+        map = new int[N][M];
+
+
+        for (int i = 0; i < N; i++) {
+            st =  new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -35,61 +33,48 @@ public class Main {
 
 
 
-
-
-
-
-
-
-
     }
-
     public static void bfs() {
         Queue<int[]> q = new LinkedList<>();
 
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if(map[i][j] == 1) {
-                    q.add(new int[]{i,j});
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1) {
+                    q.offer(new int[]{i,j});
                 }
             }
         }
 
         while (!q.isEmpty()) {
-            int [] temp = q.poll();
+            int[] temp = q.poll();
 
             for (int i = 0; i < 4; i++) {
                 int nx = temp[0] + dx[i];
                 int ny = temp[1] + dy[i];
 
-                if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-                    if (map[nx][ny] != 0) {
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (map[nx][ny] !=0) {
                         continue;
                     }
-                    map[nx][ny] = map[temp[0]][temp[1]] + 1;
-                    q.add(new int[] {nx,ny});
-
+                        map[nx][ny] = map[temp[0]][temp[1]] + 1;
+                        q.offer(new int[] {nx,ny});
                     }
                 }
             }
 
-        countday = 0;
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
+        answer = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 if(map[i][j] == 0) {
                     System.out.println(-1);
                     return;
                 }
-                countday = Math.max(countday,map[i][j]);
+                answer = Math.max(answer,map[i][j]);
             }
-
         }
-        System.out.println(countday-1);
-        }
-
-
-
-
-
+        System.out.println(answer-1);
     }
+
+}
+
 
