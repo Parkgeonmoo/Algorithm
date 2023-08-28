00f41d1b13eaae8ca4c33c5e5c0e6176868ed2b2
Parkgeonmoo <pgw111111@naver.com>
2023-08-28 11:35:53 +0900

    [Gold V] Title: 숫자고르기, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,46 +1,55 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 public class Main{
 
     static int[] arr;
     static boolean[] visited;
-    //last는 마지막 점이 출발점과 같다면 사이클이 완성 됬으므로 리스트에 추가 이게 -> dfs하려면 사이클 해야되는구나 estsoft문제 2번 문제와 유사
     static int last;
-    //ArrayList로 하나 변수를 두어야 하고
-    static ArrayList<Integer> list;
+    static ArrayList<Integer> list = new ArrayList<>();
 
-    public static void main(String[] args){
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
 
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        arr= new int[N+1];
-        visited = new boolean[N+1];
-        list = new ArrayList<Integer>();
-        for(int i=1;i<=N;i++){
-            arr[i] = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        for(int i=1;i<=N;i++){
-            visited[i] = true; //무한 재귀에 빠지면 안되므로 첫 시작점도 방문으로 체크
+
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[N+1];
+            visited[i] = true;
             last = i;
-            DFS(i);
-            visited[i] = false; //다음 숫자를 dfs해야 하니까 초기화 시켜준다.
+            bfs(i);
+
         }
-        Collections.sort(list);//작은 수 부터 출력해야 하니까 정렬
+
         System.out.println(list.size());
-        for(int i : list){
-            System.out.println(i); //list들을 하나씩 출력해준다.
+
+        for (int i : list) {
+            System.out.println(i);
         }
+
+
     }
-    public static void DFS(int i){
-        if(!visited[arr[i]]){ //이전에 방문한 점이 아니여야 한다.
-            visited[arr[i]] = true; //방문했으므로 true;
-            DFS(arr[i]);
-            visited[arr[i]] = false; //다음 DFS를 위하여 false
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+                if (!visited[arr[temp]]) {
+                    visited[arr[temp]] = true;
+                    q.offer(arr[temp]);
+                }
+            if (arr[temp] == last){
+                list.add(arr[temp]);
+            }
         }
-        if(arr[i] == last){ //마지막 점이 출발점과 같다면 사이클이 완성됬으므로 리스트에 추가
-            list.add(last);
-        }
+
     }
 }
