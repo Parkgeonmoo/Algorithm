f50f26f246c492dda41262cce725485a2dcbbc7d
Parkgeonmoo <pgw111111@naver.com>
2023-08-30 16:36:18 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 180 ms, Memory: 25572 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -7,7 +7,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
-    static int[] visited;
+    static int[] map;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,49 +16,50 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        visited = new int[100001];
+        map = new int[100001];
 
         bfs(N);
 
-    }
 
+
+    }
     public static void bfs(int start) {
-        Queue<find> q = new LinkedList<>();
-        q.offer(new find(start, 1));
-        visited[start] = 1;
+       Queue<run> q = new LinkedList<>();
+       q.offer(new run(start,1));
+       map[start] = 1;
 
-        while (!q.isEmpty()) {
-            find temp = q.poll();
-            if(temp.x+1>=0 && temp.x+1<=100000) {
-                if (visited[temp.x + 1] == 0 || visited[temp.x + 1] > temp.count + 1) {
-                    visited[temp.x + 1] = temp.count + 1;
-                    q.offer(new find(temp.x + 1, temp.count + 1));
-                }
-            }
-            if(temp.x-1>=0 && temp.x-1<=100000) {
-                if (visited[temp.x-1] == 0 || visited[temp.x-1] > temp.count+1) {
-                    visited[temp.x - 1] = temp.count + 1;
-                    q.offer(new find(temp.x - 1, temp.count + 1));
-                }
-            }
-            if(temp.x*2>=0 && temp.x*2<=100000) {
-                if (visited[temp.x*2] == 0 || visited[temp.x*2] > temp.count) {
-                    visited[temp.x * 2] = temp.count;
-                    q.offer(new find(temp.x * 2, temp.count));
-                }
-            }
-        }
-        System.out.println(visited[K]-1);
+
+       while (!q.isEmpty()) {
+           run temp = q.poll();
+
+           if (temp.x + 1 >= 0 && temp.x + 1 < 100001) {
+               if (map[temp.x + 1] == 0 || map[temp.x + 1] > temp.count+1) {
+                   map[temp.x + 1] = temp.count+1;
+                   q.offer(new run(temp.x+1,temp.count+1));
+               }
+           }
+           if (temp.x -  1 >= 0 && temp.x - 1 < 100001) {
+               if (map[temp.x - 1] == 0 || map[temp.x - 1] > temp.count+1) {
+                   map[temp.x - 1] = temp.count+1;
+                   q.offer(new run(temp.x-1,temp.count+1));
+               }
+           }
+           if (temp.x*2 >= 0 && temp.x*2 < 100001) {
+               if (map[temp.x * 2] == 0 || map[temp.x * 2] > temp.count) {
+                   map[temp.x * 2] = temp.count;
+                   q.offer(new run(temp.x*2,temp.count));
+               }
+           }
+       }
+       System.out.println(map[K]-1);
 
     }
-
 }
-
-class find {
+class run{
     int x;
     int count;
 
-    public find(int x, int count) {
+    public run(int x,int count) {
         this.x = x;
         this.count = count;
     }
