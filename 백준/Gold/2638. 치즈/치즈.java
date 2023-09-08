b792e551964e435b55fdb42ec22ef9ceba5d2f56
Parkgeonmoo <pgw111111@naver.com>
2023-09-08 14:20:42 +0900

    [Gold III] Title: 치즈, Time: 280 ms, Memory: 41272 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.java
+++ 백준/Gold/2638. 치즈/치즈.java
@@ -1,26 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
     static int[][] map;
+    static int N,M;
     static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static Queue<int[]> q = new LinkedList<>();
+    static boolean is_cheese= false;
+    static int time = 0;
+    static int[] dx ={1,-1,0,0};
+    static int[] dy ={0,0,1,-1};
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
-        visited = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -29,60 +32,64 @@ public class Main {
             }
         }
 
-        int time = 0;
         while (true) {
-            // 외부 공기 체크
+            is_cheese = false;
             visited = new boolean[N][M];
-            checkExternalAir(0, 0);
+            aircheck(0,0);
 
-            // 치즈 녹이기
-            boolean allMelted = true;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    if (map[i][j] == 1 && isMelted(i, j)) {
+                    if (map[i][j] == 1 && cheesecheck(i,j)) {
                         map[i][j] = 0;
-                        allMelted = false;
+                        is_cheese = true;
                     }
                 }
             }
 
-            if (allMelted) {
+            if (!is_cheese){
                 break;
             }
 
             time++;
+
         }
 
         System.out.println(time);
-    }
 
-    // 외부 공기 표시하기
-    public static void checkExternalAir(int x, int y) {
-        q.offer(new int[]{x, y});
-        visited[x][y] = true;
+
+
+
+    }
+    public static void aircheck(int startx,int starty) {
+        Queue<int[]> q = new LinkedList<>();
+        visited[startx][starty] = true;
+        q.offer(new int[]{startx,starty});
 
         while (!q.isEmpty()) {
-            int[] current = q.poll();
+            int[] temp = q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nx = current[0] + dx[i];
-                int ny = current[1] + dy[i];
+                int nx = temp[0] + dx[i];
+                int ny = temp[1] + dy[i];
 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && map[nx][ny] == 0) {
-                    visited[nx][ny] = true;
-                    q.offer(new int[]{nx, ny});
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (!visited[nx][ny] && map[nx][ny] == 0) {
+                        visited[nx][ny] = true;
+                        q.offer(new int[] {nx,ny});
+                    }
                 }
             }
         }
+
+
     }
 
-    // 치즈 녹이기
-    public static boolean isMelted(int x, int y) {
+    public static boolean cheesecheck(int startx,int starty){
         int airCount = 0;
 
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int nx = startx + dx[i];
+            int ny = starty + dy[i];
 
             if (nx >= 0 && nx < N && ny >= 0 && ny < M && visited[nx][ny] && map[nx][ny] == 0) {
                 airCount++;
