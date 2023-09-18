49e7fc2b2ab18061348465d8efc65d3011398b68
Parkgeonmoo <pgw111111@naver.com>
2023-09-18 12:28:50 +0900

    [level 2] Title: 스킬트리, Time: 8.09 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public int solution(String skill, String[] skillTrees) {
+
+        return (int)Arrays.stream(skillTrees)
+                .map(s -> s.replaceAll("[^]" + skill+"]",""))
+                .filter(skill::startsWith)
+                .count();
+
+    }
+}
