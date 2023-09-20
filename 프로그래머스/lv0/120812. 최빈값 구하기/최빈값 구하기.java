d76f82210c7a557faa8b0d99cd73192977607c83
Parkgeonmoo <pgw111111@naver.com>
2023-09-20 15:23:28 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.16 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,23 +1,25 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int solution(int[] array) {
         int maxCount = 0;		// 최빈값
         int answer = 0;
-        
+
         Map<Integer, Integer> map = new HashMap<>();		// HashMap 선언 : new에서 타입 파라미터 생략가능
-        
+
         for (int number : array) {
-            int count = map.getOrDefault(number, 0) + 1;
-            
-            if (count > maxCount) {		// 최빈값이 1개 일 경우
-                maxCount = count;
+
+            map.put(number,map.getOrDefault(number, 0) + 1);
+
+            if (map.get(number) > maxCount) {		// 최빈값이 1개 일 경우
+                maxCount = map.get(number);
                 answer = number;
-            } else if (count == maxCount) {		// 최빈값이 여러개(2개 이상)일 경우
+            } else if (map.get(number) == maxCount) {		// 최빈값이 여러개(2개 이상)일 경우
                 answer = -1;
             }
-            
-            map.put(number, count);		// 값 추가 : number 키와 이와 매핑된 count 값을 추가한다
+
+
         }
         return answer;
     }
