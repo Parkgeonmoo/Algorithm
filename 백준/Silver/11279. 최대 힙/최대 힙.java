29248bea217826ae81ed8552dd857e24440bf29d
Parkgeonmoo <pgw111111@naver.com>
2023-09-21 14:03:57 +0900

    [Silver II] Title: 최대 힙, Time: 364 ms, Memory: 27036 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (!pq.isEmpty()) {
+                    sb.append(pq.poll()+"\n");
+                }
+                else {
+                    sb.append(0+"\n");
+                }
+            }
+            else{
+                pq.add(x);
+            }
+        }
+
+        System.out.println(sb.toString());
+
+
+
+
+
+    }
+}
