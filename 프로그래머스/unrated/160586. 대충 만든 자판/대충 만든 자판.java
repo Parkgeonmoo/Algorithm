5f7b9f45dd2e5e75ea25c7502c8d7a71aa2af923
Parkgeonmoo <pgw111111@naver.com>
2023-09-21 16:07:33 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.42 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,3 +1,7 @@
+
+
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,31 +12,30 @@ class Solution {
     public int[] solution(String[] keymap, String[]targets) {
         int[] result = new int[targets.length];
         Map<Character,Integer> key = new HashMap<>();
-        Map<Character,Integer> target  = new HashMap<>();
 
         for (int i = 0; i < keymap.length; i++) {
             String line = keymap[i];
-            char[] temp = line.toCharArray();
+            char [] temp = line.toCharArray();
 
             for (int j = 0; j < temp.length; j++) {
                 char tempChar = temp[j];
                 if (!key.containsKey(tempChar) || j < key.get(tempChar)) {
                     key.put(tempChar,j+1);
                 }
-
             }
 
         }
 
         for (int i = 0; i < targets.length; i++) {
             String line = targets[i];
-            char[] temp = line.toCharArray();
+            char [] temp = line.toCharArray();
             int count = 0;
             for (int j = 0; j < temp.length; j++) {
                 char tempChar = temp[j];
                 if (key.containsKey(tempChar)) {
                     count += key.get(tempChar);
-                }else{
+                }
+                else{
                     count = -1;
                     break;
                 }
@@ -40,7 +43,6 @@ class Solution {
             result[i] = count;
         }
 
-
-     return result;
+    return result;
     }
 }
