bc785193476ef8029778464b5ec5ff3dfae230f8
Parkgeonmoo <pgw111111@naver.com>
2023-10-04 22:03:34 +0900

    [Silver III] Title: N과 M (1), Time: 2868 ms, Memory: 82792 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,61 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-
-
-    
-    static int[] result;
-    static int N;
-    static int M;
+    public static int answer = 0;
+    static int[]result;
     static boolean[] visited;
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[]args) throws IOException{
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         result = new int[M];
         visited = new boolean[N+1];
 
-        find (0,0);
-        System.out.println(sb.toString());
-
-
+        find(0,N,M);
 
 
     }
-
-    public static void find(int depth,int index) {
-
+    public static void find(int depth,int N,int M) {
         if (depth == M) {
             for (int temp : result) {
-                sb.append(temp +" ");
+                System.out.print(temp + " ");
             }
-            sb.append("\n");
+            System.out.println();
             return;
         }
-
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
-                result[index] = i;
                 visited[i] = true;
-                find(depth + 1, index + 1);
+                result[depth] = i;
+                find(depth+1,N,M);
                 visited[i] = false;
             }
         }
     }
 
+    }
 
 
 
 
 
-}
