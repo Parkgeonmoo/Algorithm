da8b148cb88fbe2d1d921271e921f67f10a7d2f6
Parkgeonmoo <pgw111111@naver.com>
2023-10-24 13:32:46 +0900

    [Silver III] Title: 블로그, Time: 364 ms, Memory: 35072 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -4,7 +4,6 @@ import java.io.*;
 public class Main {
     public static int total;
     public static int max;
-    public static int[] daynum;
     public static int[] day;
     public static int count = 0;
 
@@ -13,32 +12,32 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
-        day = new int[N];
-        daynum = new int[X];
-        max = 0;
 
+        int[] day = new int[N];
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < N; i++) {
             day[i] = Integer.parseInt(st.nextToken());
-            if(i < X){
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (i < X) {
                 total += day[i];
             }
-
-            if(i == X-1){
-                max=total;
-                count=1;
+            if (i == X -1) {
+                max = total;
+                count = 1;
             }
-
-            if(i >= X){
+            if (i >= X){
                 Add(day[i]);
                 Remove(day[i-X]);
 
-                if(total > max){
-                    max=total;
-                    count=1; // Reset the count when we find a new maximum
-                } else if(total == max){
-                    count++; // Increase the count when we find an equal maximum
+                if (total > max) {
+                    max = total;
+                    count = 1;
+                }
+                else if (total == max) {
+                    count++;
                 }
 
             }
@@ -46,18 +45,16 @@ public class Main {
 
         if (max == 0) {
             System.out.println("SAD");
-        } else{
+        }
+        else {
             System.out.println(max);
             System.out.println(count);
         }
 
-
     }
-
     public static void Add(int num) {
-         total += num;
+        total += num;
     }
-
     public static void Remove(int num) {
         total -= num;
     }
