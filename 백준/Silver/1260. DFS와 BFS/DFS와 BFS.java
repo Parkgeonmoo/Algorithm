ac158a5ef16ffeb9b51e634878aeb524693ee432
Parkgeonmoo <pgw111111@naver.com>
2023-11-02 11:13:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 256 ms, Memory: 20144 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,86 +1,86 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] arraylist;
-    static boolean[] dfsvisited;
-    static boolean[] bfsvisited;
-    static int N,M,V;
-    static StringBuilder dfsbuilder = new StringBuilder();
-    static StringBuilder bfsbuilder = new StringBuilder();
+
+    public static ArrayList<Integer>[] num;
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+    public static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
 
-        arraylist = new ArrayList[N+1];
-        dfsvisited = new boolean[N+1];
-        bfsvisited = new boolean[N+1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        num = new ArrayList[N+1];
+        visited = new boolean[N+1];
 
         for (int i = 0; i <= N; i++) {
-            arraylist[i] = new ArrayList<>();
+            num[i] = new ArrayList<Integer>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int vertex = Integer.parseInt(st.nextToken());
-            int edge = Integer.parseInt(st.nextToken());
-            arraylist[vertex].add(edge);
-            arraylist[edge].add(vertex);
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            num[s].add(e);
+            num[e].add(s);
         }
 
         for (int i = 1; i <= N; i++) {
-            Collections.sort(arraylist[i]);
+            Collections.sort(num[i]);
         }
-        
-        dfs(V);
-        bfs(V);
 
-
-        System.out.println(dfsbuilder.toString());
-        System.out.println(bfsbuilder.toString());
-
-
-
+        DFS(V);
+        System.out.println(sb.toString());
+        sb = new StringBuilder();
+        visited = new boolean[N+1];
+        BFS(V);
+        System.out.println(sb.toString());
 
 
 
     }
-    public static void dfs(int start) {
-        dfsvisited[start] = true;
-        dfsbuilder.append(start + " ");
-        for (int i : arraylist[start]) {
-             if (!dfsvisited[i]) {
-                 dfsvisited[i] = true;
-                 dfs(i);
-                }
 
+    public static void DFS(int start) {
+        sb.append(start + " ");
+        visited[start] = true;
+
+        for (int i : num[start]) {
+            if (!visited[i]) {
+                DFS(i);
+            }
         }
-
     }
-    public static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        bfsvisited[start] = true;
-        bfsbuilder.append(start + " ");
-        queue.offer(start);
 
-        while (!queue.isEmpty()) {
-            int check  = queue.poll();
-            for (int i : arraylist[check]) {
-                if (!bfsvisited[i]) {
-                    bfsvisited[i] = true;
-                    bfsbuilder.append(i + " ");
-                    queue.add(i);
+    public static void BFS(int start) {
+        Queue<Integer> q = new LinkedList();
+        visited[start] = true;
+        sb.append(start+ " ");
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+
+            for (int i : num[temp]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    q.add(i);
+                    sb.append(i + " ");
                 }
             }
-
         }
     }
+
+
+
+
+
 }
