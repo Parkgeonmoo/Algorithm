b1edb40c7fe3298654989ac2cd1d224037fc8733
Parkgeonmoo <pgw111111@naver.com>
2023-11-02 10:55:42 +0900

    [Gold V] Title: ABCDE, Time: 280 ms, Memory: 20644 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,56 +1,73 @@
-import java.util.*;
-import java.io.*;
 
-public class Main{
-    public static boolean visited[];
-    public static boolean arrive;
-    public static ArrayList<Integer>[] map;
-    public static void main(String[]args)throws IOException {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static ArrayList<Integer>[] num;
+    public static boolean[] visited;
+    public static boolean flag = false;
+    public static int N;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        arrive =false;
-        map = new ArrayList[N];
+
+        num = new ArrayList[N];
         visited = new boolean[N];
 
         for (int i = 0; i < N; i++) {
-            map[i] = new ArrayList<Integer>();
+            num[i] = new ArrayList<Integer>();
         }
 
-        for (int i = 0; i <M; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            map[a].add(b);
-            map[b].add(a);
+
+            num[a].add(b);
+            num[b].add(a);
         }
+
 
         for (int i = 0; i < N; i++) {
-            DFS(i,1);
-            if (arrive) {
-                break;
-            }
+                DFS(i,1);
+                if (flag) {
+                    break;
+                }
+
         }
 
-        if (arrive)
-            System.out.println("1");
-        else
-            System.out.println("0");
+        if (flag) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
 
     }
-    public static void DFS(int v,int depth) {
-        if (depth == 5){
-            arrive = true;
+
+    public static void DFS(int start,int length) {
+        if (length == 5) {
+            flag = true;
             return;
         }
+        visited[start] = true;
 
-        visited[v] = true;
-        for (int i : map[v]){
+        for (int i : num[start]) {
             if (!visited[i]) {
-                DFS(i,depth+1);
+                DFS(i,length+1);
             }
         }
-        visited[v] = false;
+        visited[start] = false;
+
     }
+
+
 }
