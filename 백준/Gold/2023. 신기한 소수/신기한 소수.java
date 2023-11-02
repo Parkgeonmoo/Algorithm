205570ed5869a53750f596b2f1954350dd71cd4b
Parkgeonmoo <pgw111111@naver.com>
2023-11-02 10:39:07 +0900

    [Gold V] Title: 신기한 소수, Time: 820 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,36 +1,46 @@
-import java.util.*;
+
 import java.io.*;
 
-public class Main{
+public class Main {
+    
     public static int N;
-    public static void main(String[]args) {
-        Scanner in = new Scanner(System.in);
-        N = in.nextInt();
-        DFS(2,1);
-        DFS(3,1);
-        DFS(5,1);
-        DFS(7,1);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        DFS(2, 1);
+        DFS(3, 1);
+        DFS(5, 1);
+        DFS(7, 1);
+
+
     }
-    public static void DFS(int num,int jarisu) {
-        if (jarisu == N){
-            if (isPrime(num)) {
-                System.out.println(num);
-            }
-            return;
-        }
-        for (int i = 1; i < 10; i++) {
-            if (i % 2 == 0){
-                continue;
-            }
-            if (isPrime(num*10 + i)) {
-                DFS(num*10+i,jarisu+1);
-            }
-        }
+
+    public static void DFS(int total, int length) {
+         if (length == N) {
+             if (is_prime(total)) {
+                 System.out.println(total);
+             }
+             return;
+         }
+
+         for (int i = 1; i < 10; i++) {
+             if (i % 2 == 0) {
+                 continue;
+             }
+             if (is_prime(total*10 + i)) {
+                 DFS(total * 10 + i, length + 1);
+             }
+         }
     }
-    static boolean isPrime(int num) {
-        for (int i = 2; i <= num/2; i++) {
-            if (num % i == 0)
+
+    public static boolean is_prime(int total) {
+
+        for (int i = 2; i <= total / 2; i++) {
+            if (total % i == 0) {
                 return false;
+            }
         }
         return true;
     }
