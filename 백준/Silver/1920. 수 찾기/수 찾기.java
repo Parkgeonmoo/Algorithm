e7a50fe368e0ed5c60f5c5e591a396ea2dfad248
Parkgeonmoo <pgw111111@naver.com>
2023-11-03 15:01:56 +0900

    [Silver IV] Title: 수 찾기, Time: 2136 ms, Memory: 188924 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,40 +1,53 @@
-import java.util.*;
 
-public class  Main {
+import java.util.Scanner;
+import java.util.Arrays;
 
-    public static void main(String[] args) {
+public class Main{
+    static int M,N;
+    static int[] map,check;
+
+    public static void main(String[]args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int [] A = new int[N];
-        for (int i = 0; i < N; i++){
-            A[i] = in.nextInt();
-        }
-        Arrays.sort(A);
+        N = in.nextInt();
+        map = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            map[i] = in.nextInt();
+        } // 기본 배열 입력
+        Arrays.sort(map); // 맵 정렬
+
         int M = in.nextInt();
-        for (int i = 0; i < M; i++){
+        check = new int[M];
+
+        for (int i = 0; i < M; i++) {
             boolean find = false;
             int target = in.nextInt();
-            int start = 0;
-            int end = A.length-1;
-            while(start <= end) {
-                int midi = (start+end)/2;
-                int midv = A[midi];
-                if(midv > target) {
-                    end = midi - 1;
+            //찾아야 할 숫자 배열 입력
 
-                } else if (midv < target) {
-                    start = midi + 1;
+            int start = 0;
+            int end = map.length - 1;
+            while (start <= end) {
+                int midi = (start+end)/2;
+                int midV = map[midi];
+                if(midV > target) {
+                    end = midi -1;
+                }
+                else if(midV < target){
+                    start = midi +1;
                 }else{
-                    find = true;
+                    find= true;
                     break;
                 }
             }
-            if (find) {
+            if(find) {
                 System.out.println(1);
             }else{
                 System.out.println(0);
             }
+
         }
+
+
 
     }
 }
