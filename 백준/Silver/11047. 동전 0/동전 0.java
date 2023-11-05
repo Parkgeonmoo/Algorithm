271d95ec1c70dd244ef7e1540470a4ed53dc2d39
Parkgeonmoo <pgw111111@naver.com>
2023-11-05 15:12:23 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,23 +1,41 @@
-import java.util.*;
 
-public class  Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int K = in.nextInt();
-        int[]A = new int[N];
-        for (int i = 0; i < N; i++){
-            A[i] = in.nextInt();
-        }
+public class Main{
+
+    public static void main(String[]args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int [] coins = new int[N];
         int count = 0;
-        for (int i = N-1; i >= 0; i--){
-            if(A[i] <= K){
-                count += (K / A[i]);
-                K = K % A[i];
+
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+
+
+
+        for (int i = coins.length - 1; i >= 0; i--) {
+            int coinPrice = coins[i];
+            int nowCount = 0;
+            if (coinPrice <= K) {
+                count += K / coinPrice;
+                nowCount = K /coinPrice;
+                K -= nowCount * coinPrice;
+            } else {
+                continue;
             }
         }
         System.out.println(count);
-    }
 
+
+
+    }
 }
