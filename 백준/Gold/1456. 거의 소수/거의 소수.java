7bfd1e156f5db80e24bb005b27b2b3e907a22361
Parkgeonmoo <pgw111111@naver.com>
2023-11-06 11:24:42 +0900

    [Gold V] Title: 거의 소수, Time: 500 ms, Memory: 96048 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.java
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -1,36 +1,41 @@
 import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[]args) {
+        Scanner in = new Scanner(System.in);
+        long A = in.nextLong();
+        long B = in.nextLong();
+        long[] map = new long[10000001];
+
+        for (int i = 2; i < map.length; i++) {
+            map[i] = i;
+        }
+
+        for (int i = 2; i <= Math.sqrt(map.length); i++) {
+            if (map[i] == 0) {
+                continue;
+            }
+            for (int j = i+i; j < map.length; j = j+i) {
+                map[j] = 0;
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 2; i < map.length; i++) {
+            if (map[i] != 0) {
+                long temp = map[i];
+                while ((double) map[i] <= (double) B / (double) temp) {
+                    if ((double) map[i] >= (double) A / (double) temp) {
+                        count++;
+                    }
+                    temp = temp * map[i];
+                }
+            }
+        }
+        System.out.println(count);
 
 
-public class  Main {
-
-    public static void main(String[] args) {
-      Scanner in = new Scanner(System.in);
-      long Min = in.nextLong();
-      long Max = in.nextLong();
-      long[] A = new long[10000001];
-      for (int i = 2; i < A.length; i++) {
-          A[i] = i;
-      }
-      for (int i = 2; i <=Math.sqrt(A.length); i++) {
-          if( A[i] == 0) {
-              continue;
-          }
-          for (int j = i+i; j < A.length; j=j+i) {
-              A[j] = 0;
-          }
-      }
-      int count = 0;
-      for (int i = 2; i < 10000001; i++) {
-          if (A[i] != 0) {
-              long temp = A[i];
-              while((double)A[i] <= (double)Max / (double)temp) {
-                  if((double)A[i] >= (double)Min /(double)temp) {
-                      count++;
-                  }
-                  temp = temp *A[i];
-              }
-          }
-      }
-      System.out.println(count);
     }
 }
