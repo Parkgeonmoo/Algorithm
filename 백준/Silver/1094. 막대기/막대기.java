d9364a95f50cd1fe52b4e067895cd24cbc43b15a
Parkgeonmoo <pgw111111@naver.com>
2023-11-06 21:10:52 +0900

    [Silver V] Title: 막대기, Time: 132 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int count = 0;
+        int stick = 64;
+
+        while (X > 0) {
+            if (stick > X) {
+               stick = stick >> 1;
+            }
+            else{
+                X -= stick;
+                count++;
+            }
+
+
+        }
+
+        System.out.println(count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
