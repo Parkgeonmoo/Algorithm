7f8a435fb62a79c26a460c3d3d50d638e9736899
Parkgeonmoo <pgw111111@naver.com>
2023-11-06 10:26:24 +0900

    [Silver III] Title: 소수 구하기, Time: 900 ms, Memory: 34252 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,28 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
+public class Main {
+    public static boolean[] check;
+    public static int[] num;
 
-public class  Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int M = in.nextInt();
-        int N = in.nextInt();
-        int[] A = new int [N+1];
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        num = new int[N+1];
+
         for (int i = 2; i <= N; i++) {
-            A[i] = i;
-
+            num[i] = i;
         }
+
         for (int i = 2; i <= Math.sqrt(N); i++) {
-            if(A[i] == 0) {
+            if (num[i] == 0) {
                 continue;
             }
             for (int j = i+i; j <= N; j = j+i) {
-                A[j] = 0;
+                num[j] = 0;
             }
         }
+
         for (int i = M; i <= N; i++) {
-            if(A[i] != 0) {
-                System.out.println(A[i]);
+            if (num[i] != 0) {
+                System.out.println(num[i]);
             }
         }
 
