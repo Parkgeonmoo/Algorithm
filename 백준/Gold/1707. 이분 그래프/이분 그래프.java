6e2a86a08c62628871b37ca06747b346893721ff
Parkgeonmoo <pgw111111@naver.com>
2023-11-08 21:47:59 +0900

    [Gold IV] Title: 이분 그래프, Time: 1004 ms, Memory: 269524 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,60 +1,72 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main{
+public class Main {
+    public static ArrayList<Integer>[] list;
     public static boolean[] visited;
-    public static ArrayList<Integer>[] map;
-    public static int[] check;
-    public static boolean IsEven;
-    public static void main(String[]args)throws IOException {
+    public static int[]checked;
+    public static boolean IsEven = true;
+
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
+        int K = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
-
-        for (int t  = 0; t < N; t++) {
-            String[] s = br.readLine().split(" ");
-            int S = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            map = new ArrayList[S+1];
-            visited = new boolean[S+1];
-            check = new int[S+1];
+            list = new ArrayList[V+1];
+            visited = new boolean[V+1];
+            checked = new int[V+1];
             IsEven = true;
-            for (int i = 1; i <= S; i++) {
-                map[i] = new ArrayList<Integer>();
-            }
-            for (int i = 0; i < E; i++) {
-                s = br.readLine().split(" ");
-                int start = Integer.parseInt(s[0]);
-                int end = Integer.parseInt(s[1]);
-                map[start].add(end);
-                map[end].add(start);
-            }
-            for (int i = 1; i <= S; i++) {
-                if(IsEven)
-                    DFS(i);
-                else
-                    break;
-            }
-            if (IsEven)
-                System.out.println("YES");
-            else
-                System.out.println("NO");
 
-        }
-    }
-    public static void DFS(int node) {
-        visited[node] = true;
-        for (int i : map[node]) {
-            if (!visited[i]) {
-                check[i] = (check[node]+1) % 2;
-                DFS(i);
+            for (int l = 0; l <= V; l++) {
+                list[l] = new ArrayList<Integer>();
             }
-            else if (check[node] == check[i]){
+
+            for (int j = 0; j < E; j++) {
+                st = new StringTokenizer(br.readLine());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+
+                list[u].add(v);
+                list[v].add(u);
+
+            }
+
+            for (int k = 1; k <= V; k++) {
+                if (IsEven) {
+                    DFS(k);
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (IsEven) {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+
+    }
+    public static void DFS(int start) {
+        visited[start] = true;
+
+        for (int temp : list[start]) {
+            if (!visited[temp]) {
+                checked[temp] = (checked[start]+1) % 2;
+                DFS(temp);
+            }
+            else if (checked[start] == checked[temp]){
                 IsEven = false;
             }
         }
