b9d7f1a3bac5044c7dab145584687c364436d11f
Parkgeonmoo <pgw111111@naver.com>
2023-11-10 18:19:53 +0900

    [Gold V] Title: 집합의 표현, Time: 1300 ms, Memory: 53344 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,46 +1,66 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main{
-
+public class Main {
     public static int[] parent;
-    public static void main(String[]args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int M = in.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
         parent = new int[N+1];
 
-        for (int i = 0 ; i <= N; i++) {
+        for (int i = 0; i <= N; i++) {
             parent[i] = i;
         }
 
-        for (int i = 0; i < M ;i++) {
-            int question = in.nextInt();
-            int a = in.nextInt();
-            int b = in.nextInt();
-            if (question == 0) {
-                union(a,b);
-            }else{
-                if (checkSame(a,b)) {
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int operation = Integer.parseInt(st.nextToken());
+            int numOne = Integer.parseInt(st.nextToken());
+            int numTwo = Integer.parseInt(st.nextToken());
+
+            if (operation == 0) {
+                union(numOne,numTwo);
+            }
+            else {
+                if (checkSame(numOne,numTwo)) {
                     System.out.println("YES");
-                }else{
+                }
+                else {
                     System.out.println("NO");
                 }
             }
         }
-    }
-    public static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            parent[b] = a;
+
+
+
+
         }
-    }
-    public static int find(int a) {
-        if (a == parent[a])
-            return a;
-        else
-            return parent[a] = find(parent[a]);
-    }
+
+        public static void union(int numOne,int numTwo) {
+          numOne = find(numOne);
+          numTwo = find(numTwo);
+
+          if (numOne != numTwo) {
+              parent[numTwo] = numOne;
+          }
+        }
+
+        public static int find(int num) {
+            if (parent[num] == num) {
+                return num;
+            }
+            else {
+                return parent[num] = find(parent[num]);
+            }
+        }
+
     public static boolean checkSame(int a,int b) {
         a = find(a);
         b = find(b);
@@ -49,4 +69,4 @@ public class Main{
         }
         return false;
     }
-}
+    }
