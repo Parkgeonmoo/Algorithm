3927fff69c3e57c4a11f593a5cce3156e47ef0fb
Parkgeonmoo <pgw111111@naver.com>
2023-11-13 21:25:33 +0900

    [Gold V] Title: 물통, Time: 140 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -1,66 +1,88 @@
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int[] Sender = {0,0,1,1,2,2};
-    static int[] Receiver = {1,2,0,2,0,1};
-    static boolean visited[][];
-    static boolean answer[];
-    static int now[];
+    public static int[] Receiver = {0,0,1,1,2,2};
+    public static int[] sender = {1,2,0,2,0,1};
+    static boolean answer[] = new boolean[201];
+    public static int[] now;
+    public static boolean[][] visited = new boolean[201][201];
 
-    public static void main(String[]args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
         now = new int[3];
-        now[0] = in.nextInt();
-        now[1] = in.nextInt();
-        now[2] = in.nextInt();
-        visited = new boolean[201][201];
-        answer = new boolean[201];
+        now[0] = A;
+        now[1] = B;
+        now[2] = C;
+
         BFS();
+
         for (int i = 0; i < answer.length; i++) {
-            if (answer[i]) System.out.print(i + " ");
+            if (answer[i]) {
+                System.out.print(i + " ");
+            }
         }
+
+
+
+
+
+
+
+
+
+
+
 
     }
     public static void BFS() {
-        Queue<AB> queue = new LinkedList<>();
-        queue.add(new AB(0,0));
-        visited[0][0] = true;
+        Queue<AB> q = new LinkedList<>();
+        q.add(new AB(0,0));
+        visited[0][0] =true;
         answer[now[2]] = true;
-        while (!queue.isEmpty()) {
-            AB p = queue.poll();
-            int A = p.A;
-            int B = p.B;
+
+        while (!q.isEmpty()) {
+            AB temp = q.poll();
+            int A = temp.A;
+            int B = temp.B;
             int C = now[2] - A - B;
-            for (int k = 0; k < 6; k++) {
-                int[]next = {A,B,C};
-                next[Receiver[k]] += next[Sender[k]];
-                next[Sender[k]] = 0;
-                if (next[Receiver[k]] > now[Receiver[k]]) {
-                    next[Sender[k]] = next[Receiver[k]] - now[Receiver[k]];
-                    next[Receiver[k]] = now[Receiver[k]];
-                }
-                if (!visited[next[0]][next[1]]) {
-                    visited[next[0]][next[1]] = true;
-                    queue.add(new AB(next[0],next[1]));
-                    if (next[0] == 0) {
-                        answer[next[2]] =true;
-                    }
-                }
+
+            for (int i = 0; i < 6; i++) {
+                int[] next = {A,B,C};
+                next[Receiver[i]] += next[sender[i]];
+                next[sender[i]] = 0;
+
+                 if (next[Receiver[i]] > now[Receiver[i]]) {
+                     next[sender[i]] = next[Receiver[i]] - now[Receiver[i]];
+                     next[Receiver[i]] = now[Receiver[i]];
+                 }
+                 if (!visited[next[0]][next[1]]) {
+                     visited[next[0]][next[1]] = true;
+                     q.add(new AB(next[0],next[1]));
+                     if (next[0] == 0) {
+                         answer[next[2]] = true;
+                     }
+                 }
+
             }
         }
     }
-}
-class AB{
-    int A;
-    int B;
-    public AB(int A,int B){
-        this.A = A;
-        this.B = B;
+    static class AB{
+        int A;
+        int B;
+        public AB(int A,int B){
+            this.A = A;
+            this.B = B;
+        }
     }
 }
+
+
+
