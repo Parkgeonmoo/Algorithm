13a8282abe1e04ec3cd7b816c6503f47343a8bbd
Parkgeonmoo <pgw111111@naver.com>
2023-11-13 22:18:51 +0900

    [Gold IV] Title: 거짓말, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,75 +1,91 @@
-import java.sql.Array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main{
-    public static int[] truep;
-    public static ArrayList<Integer>[] party;
+public class Main {
+
+    public static int[] truth;
     public static int[] parent;
-    public static int result;
+    public static ArrayList<Integer>[] people;
+    public static int count = 0;
 
-    public static void main(String[]args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int M = in.nextInt();
-        int t = in.nextInt();
-        result =0;
-        truep = new int[t];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < t; i++) {
-            truep[i] = in.nextInt();
-        }
-
-        party = new ArrayList[M];
-        for (int i = 0; i < M; i++) {
-            party[i] = new ArrayList<Integer>();
-            int party_size = in.nextInt();
-            for (int j = 0; j < party_size; j++) {
-                party[i].add(in.nextInt());
-            }
-        }
+        truth = new int[t];
         parent = new int[N+1];
-        for (int i = 0; i<= N; i++) {
+
+        for (int i = 0; i <= N; i++) {
             parent[i] = i;
         }
 
+        for (int i = 0; i < t; i++) {
+            truth[i] = Integer.parseInt(st.nextToken());
+        }
+
+        people = new ArrayList[M];
+
         for (int i = 0; i < M; i++) {
-            int fristpeople = party[i].get(0);
-            for (int j = 1; j < party[i].size(); j++) {
-                union(fristpeople,party[i].get(j));
+            st = new StringTokenizer(br.readLine());
+            int listSize = Integer.parseInt(st.nextToken());
+            people[i] = new ArrayList<>();
+
+            for (int j = 0; j < listSize; j++) {
+                people[i].add(Integer.parseInt(st.nextToken()));
             }
         }
+
+        for (int i = 0; i < M; i++) {
+            int first = people[i].get(0);
+
+            for (int j = 1; j < people[i].size(); j++) {
+                union(first,people[i].get(j));
+            }
+        }
+
+
         for (int i = 0; i < M; i++) {
             boolean isPossible =  true;
-            int cur = party[i].get(0);
-            for (int j = 0; j < truep.length; j++) {
-                if (find(cur) == find(truep[j])) {
+            int cur = people[i].get(0);
+            for (int j = 0; j < truth.length; j++) {
+                if (find(cur) == find(truth[j])) {
                     isPossible = false;
                     break;
                 }
             }
-            if (isPossible) result++;
+            if (isPossible) count++;
         }
-        System.out.println(result);
+        System.out.println(count);
+
+
+
     }
-    public static void union(int a,int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b){
-            parent[b] = a;
-        }
-    }
-    public static int find(int a) {
-        if (a == parent[a])
-            return a;
-        else
-            return parent[a] = find(parent[a]);
-    }
-    public static boolean checkSame(int a,int b) {
-        a = find(a);
-        b = find(b);
-        if (a == b) {
-            return true;
-        }
-        return false;
+
+    public static void union(int A,int B) {
+        A = find(A);
+        B = find(B);
+
+        if (A != B) {
+            parent[B] = A;
         }
     }
+
+    public static int find(int num) {
+        if (parent[num] == num) {
+            return num;
+        }
+        else {
+            return parent[num] = find(parent[num]);
+        }
+    }
+}
+
+
+
