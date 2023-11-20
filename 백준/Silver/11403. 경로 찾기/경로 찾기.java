78733dc56eb4fc32e2547e3a3ae6fb964334b49a
Parkgeonmoo <pgw111111@naver.com>
2023-11-20 20:53:50 +0900

    [Silver I] Title: 경로 찾기, Time: 372 ms, Memory: 18656 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static int[][] map;
+    public static int N;
+
+    public static void main(String[]args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N+1][N+1];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int v = Integer.parseInt(st.nextToken());
+                map[i][j] = v;
+            }
+        }
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (map[i][k] == 1 && map[k][j] == 1)
+                        map[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
