0cc4cd4d7e96bd08d3f6a00df26cf080a3f59a2a
Parkgeonmoo <pgw111111@naver.com>
2023-12-07 11:40:13 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,27 +1,34 @@
-import java.util.Scanner;
 
-public class Main{
-    static int N,K;
-    static int [][] map;
-    public static void main(String[]args) {
-        Scanner in = new Scanner(System.in);
-        N = in.nextInt();
-        K = in.nextInt();
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int[][] map;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         map = new int[11][11];
 
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i < map.length; i++) {
             map[i][i] = 1;
             map[i][0] = 1;
-            map[i][1] = i;
+            map[i][i] = i;
         }
 
-        for (int i = 2; i <= N; i++) {
-            for (int j = 1; j < i; j++) {
+        for (int i = 2; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
                 map[i][j] = map[i-1][j] + map[i-1][j-1];
             }
-
         }
+
         System.out.println(map[N][K]);
+
+
 
 
     }
