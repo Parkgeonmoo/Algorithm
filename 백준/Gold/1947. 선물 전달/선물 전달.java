b501bb801449ec32a9c1a1ed06968a3821b01888
Parkgeonmoo <pgw111111@naver.com>
2023-12-12 16:46:29 +0900

    [Gold III] Title: 선물 전달, Time: 152 ms, Memory: 22100 KB -BaekjoonHub

--- 백준/Gold/1947. 선물 전달/선물 전달.java
+++ 백준/Gold/1947. 선물 전달/선물 전달.java
@@ -1,18 +1,30 @@
-import java.util.*;
 
-public class Main{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    public static void main(String[]args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        long[] map = new long[1000001];
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] D = new long[1000001];
         long mod = 1000000000;
-        map[1] = 0;
-        map[2] = 1;
+
+        D[0] = 0;
+        D[1] = 0;
+        D[2] = 1;
+
+        //내가 선물을 양뱡향으로 교환하거나 또는 선물을 단방향으로 교환하는 방법
 
         for (int i = 3; i <= N; i++) {
-            map[i] = (i-1) * (map[i-1] +map[i-2]) % mod;
+            D[i] = ((D[i-1] + D[i-2]) * (i-1)) % mod;
         }
-        System.out.println(map[N]);
+
+        System.out.println(D[N]);
+
+
+
+
     }
 }
