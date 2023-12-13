0a3a44650e30f307330c7655ea672845f7f7f0f2
Parkgeonmoo <pgw111111@naver.com>
2023-12-13 20:46:02 +0900

    [Silver V] Title: 거스름돈, Time: 136 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -1,26 +1,45 @@
-
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+ 
+ 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int count = 0;
-        int temp = 0;
-        while(true){
-            if(N%5 == 0){
-                count += N/5;
-                System.out.println(count);
-                break;
-            }else{
-                N -= 2;
-                count++;
-            }
-            if(N < 0){
-                System.out.println(-1);
-                break;
-            }
-        }
+	
+	public static int N;
+	public static int[] dp;
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken());
+    	
+    	dp = new int[100001];
+    	dp[0] = 0;
+    	dp[1] = 0;
+    	dp[2] = 1;
+    	dp[3] = 0;
+    	dp[4] = 2;
+    	dp[5] = 1;
+    	
+    	for(int i=6; i<=N;i++) {
+    		if(dp[i-2] == 0 && dp[i-5] == 0 ) {
+    			dp[i] = 0;
+    		}
+    		else if(dp[i-2] > 0 && dp[i-5] == 0 ) {
+    			dp[i] = dp[i-2] + 1;
+    		}
+    		else if(dp[i-2] == 0 && dp[i-5] > 0 ) {
+    			dp[i] = dp[i-5] + 1;
+    		}else {
+    			dp[i] = Math.min(dp[i-2], dp[i-5]) + 1;
+    		}
+    	}
+    	
+    	if(dp[N] == 0)
+    		System.out.println("-1");
+    	else
+    		System.out.println(dp[N]);
+    	
     }
 }
