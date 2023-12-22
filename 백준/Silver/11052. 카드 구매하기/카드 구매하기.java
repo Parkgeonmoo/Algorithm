0f6749f3cd9977c73b619f6d4989ec4420ede995
Parkgeonmoo <pgw111111@naver.com>
2023-12-22 11:25:04 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] P = new int[N + 1];
+        int[] D = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                D[i] = Math.max(D[i], P[j] + D[i - j]);
+            }
+        }
+
+        System.out.println(D[N]);
+    }
+}
