2cdd16b12866b6a7239618ae13dffa21d87e9f1c
Parkgeonmoo <pgw111111@naver.com>
2023-12-27 10:20:21 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,7 @@
+SELECT ANIMAL_ID, NAME,
+  CASE 
+    WHEN SEX_UPON_INTAKE = 'Neutered Male' THEN 'O'
+    WHEN SEX_UPON_INTAKE = 'Spayed Female' THEN 'O'
+    ELSE 'X'
+  END AS 중성화
+FROM ANIMAL_INS;
