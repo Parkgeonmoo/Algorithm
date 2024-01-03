97ada054eda12e5fa033be88ec51bd6457a60b18
Parkgeonmoo <pgw111111@naver.com>
2024-01-03 11:24:03 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1384 ms, Memory: 58228 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,48 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        coordinate[] ans = new coordinate[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            ans[i] = new coordinate(x, y);
+        }
+        Arrays.sort(ans);
+
+        for (coordinate temp : ans) {
+            System.out.println(temp.x + " " + temp.y);
+        }
+    }
+
+    static class coordinate implements Comparable<coordinate> {
+        int x;
+        int y;
+
+        public coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(coordinate other) {
+            if (this.y == other.y) {
+                return Integer.compare(this.x, other.x);
+            } else {
+                return Integer.compare(this.y, other.y);
+            }
+        }
+    }
+}
