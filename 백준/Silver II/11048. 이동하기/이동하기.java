8350c1d8bf4d8159a0ab06bc87b27f2ea1a8106a
Parkgeonmoo <pgw111111@naver.com>
2024-02-15 15:51:24 +0900

    [Silver II] Title: 이동하기, Time: 524 ms, Memory: 76932 KB -BaekjoonHub

---
+++ 백준/Silver II/11048. 이동하기/이동하기.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[N+1][M+1];
+        int[][] dp = new int[N+1][M+1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = Math.max(Math.max(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + map[i][j];
+            }
+        }
+
+        System.out.println(dp[N][M]);
+    }
+
+}
