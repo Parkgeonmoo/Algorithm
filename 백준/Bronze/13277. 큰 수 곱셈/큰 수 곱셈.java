d3ffc1af28f89bbaa02f040a6ff03d6fcdac68bc
Parkgeonmoo <pgw111111@naver.com>
2024-02-21 14:02:15 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6244 ms, Memory: 395540 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		System.out.println(a.multiply(b));
+		sc.close();
+	}
+}
