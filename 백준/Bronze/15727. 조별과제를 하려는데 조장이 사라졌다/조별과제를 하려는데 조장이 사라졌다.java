69785d185f15b36f25880eb4bfd0dc9f09bd8c1f
Parkgeonmoo <pgw111111@naver.com>
2024-02-25 16:19:36 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if (N % 5 > 0) {
+            System.out.println((N / 5 + 1));
+
+        }
+        else {
+            System.out.println(N / 5);
+        }
+
+    }
+}
