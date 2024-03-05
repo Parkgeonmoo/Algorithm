f747c59fd55160f97996dedb9b9e4edaba547506
Parkgeonmoo <pgw111111@naver.com>
2024-03-05 18:20:09 +0900

    [Bronze V] Title: 제리와 톰, Time: 152 ms, Memory: 16108 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        System.out.print(q-p + " " + q);
+    }
+}
